ecdcc50554821c2219a6f95b06a396d1060ea8cf
Al-mizan <mdalmizanakon@gmail.com>
2024-09-02 15:44:33 +0600

    added comment

--- petCareApp/src/com/cse/ju/oop/Application.java
+++ petCareApp/src/com/cse/ju/oop/Application.java
@@ -5,6 +5,7 @@ import com.cse.ju.oop.views.screens.HomeScreen;
 public class Application {
     public static void main(String[] args) {
         HomeScreen homeScreen = new HomeScreen();
+        //Added comment
         homeScreen.setVisible(true);
     }
 }
