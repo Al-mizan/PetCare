1d6cccac19922490acdd67c4f74ada2196c152a7
Al-mizan <mdalmizanakon@gmail.com>
2024-10-15 10:42:34 +0600

    change Application

--- petCareApp/src/com/cse/ju/oop/Application.java
+++ petCareApp/src/com/cse/ju/oop/Application.java
@@ -1,23 +1,14 @@
-
 package petCareApp.src.com.cse.ju.oop;
 import petCareApp.src.com.cse.ju.oop.views.screens.LoginScreen;
 import petCareApp.src.com.cse.ju.oop.views.screens.RegistrationScreen;
 
 public class Application {
     public static void main(String[] args) {
-        // Create and display the login screen
         javax.swing.SwingUtilities.invokeLater(Application::loginScr);
-        javax.swing.SwingUtilities.invokeLater(Application::RegistrationScr);
     }
 
     private static void loginScr() {
-        // if you want to registration show then comment out new LoginScreen();
-//        new LoginScreen();
+        new LoginScreen();
     }
-
-    private static void RegistrationScr() {
-        new RegistrationScreen();
-    }
-
 
 }
